5e99bc6c2c6dc0a8fb633ce6a753edd8a420560b
HyundeokPark <hpotter1993@naver.com>
2022-11-12 09:26:43 +0900

    chore - refactoring

--- src/com/company/programmers/lv2/Lesson42586.java
+++ src/com/company/programmers/lv2/Lesson42586.java
@@ -9,21 +9,33 @@ import java.util.stream.Collectors;
 
 public class Lesson42586 {
 
+    /**
+     * [93, 30, 55]	[1, 30, 5]	[2, 1]
+     * [95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
+     * @param args
+     */
     public static void main(String[] args) {
         Lesson42586 lesson = new Lesson42586();
-        int[] progresses = {1};
-        int[] speeds = {1};
-        lesson.solution(progresses,speeds);
+        int[] progresses = {93,30,55};
+        int[] speeds = {1,30,5};
+        Arrays.stream(lesson.solution(progresses,speeds)).forEach(i-> System.out.println(i));
     }
 
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
         List<Integer> deployedWork = new ArrayList<>();
         List<Worker> workers = new LinkedList<>();
+        for(int i=0; i<progresses.length; i++){
+            Worker worker = new Worker(progresses[i],speeds[i]);
+            workers.add(worker);
+        }
+
+
+
         int count = 0;
         while(!workers.isEmpty()){
             this.recursive(workers,count);
-            if(count>=0){
+            if(count>=1){
                 deployedWork.add(count);
             }
             this.progressAllWorker(workers);
@@ -32,14 +44,15 @@ public class Lesson42586 {
         answer = deployedWork.stream().mapToInt(i->i).toArray();
         return answer;
     }
-    public void recursive(List<Worker> workers, int count){
+    public int recursive(List<Worker> workers, int count){
         int workCount = count;
         if(workers.get(0).isDone()){
             workers.remove(0);
 //            workCount++;
             count++;
-            recursive(workers, count);
+            count = recursive(workers, count);
         }
+        return count;
 //        return workCount;
     }
     public void progressAllWorker(List<Worker> workers){
@@ -51,6 +64,10 @@ public class Lesson42586 {
         int work;
         int speed;
 
+        public Worker(int work, int speed){
+            this.work = work;
+            this.speed = speed;
+        }
         public void progressWork(){
             work+=speed;
         }
