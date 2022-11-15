42eaea468b73778d3b01cf1269589260907047b7
HyundeokPark <hpotter1993@naver.com>
2022-11-15 23:39:07 +0900

    wip : set -> tuple how?..

--- src/com/company/programmers/lv2/Lesson64065.java
+++ src/com/company/programmers/lv2/Lesson64065.java
@@ -10,7 +10,13 @@ public class Lesson64065 {
         Arrays.stream(lesson.solution(s)).forEach(System.out::println);
     }
 
-    
+    /**
+     * 1. 집합의 순서는 바뀌어도 되나, 이를 통해서 튜플을 확인하는게 굉장히 이상하다. -> 이해가 안된다.
+     * 2. 순서가 바뀌어도 되므로, 튜플의 순서를 유추해 내는것이 사실상 불가능하지 않은가?
+     * 3. 그저 튜플을 이루는 원소들만 중복제거로 알 수있을것 같다.
+     * @param s
+     * @return
+     */
     public int[] solution(String s) {
         int[] answer = {};
         return answer;
