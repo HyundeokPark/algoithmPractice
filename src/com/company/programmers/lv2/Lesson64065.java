4c800c2488969313833a68271586f037acbda047
HyundeokPark <hpotter1993@naver.com>
2022-11-15 23:01:07 +0900

    init

---
+++ src/com/company/programmers/lv2/Lesson64065.java
@@ -0,0 +1,19 @@
+package com.company.programmers.lv2;
+
+import java.util.Arrays;
+
+public class Lesson64065 {
+
+    public static void main(String[] args) {
+        Lesson64065 lesson = new Lesson64065();
+        String s = "";
+        Arrays.stream(lesson.solution(s)).forEach(System.out::println);
+    }
+
+    
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
