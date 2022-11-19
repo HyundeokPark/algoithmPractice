3bdd236a09cb33dfe56de4e5719c0e89f8d7a767
HyundeokPark <hpotter1993@naver.com>
2022-11-19 10:59:05 +0900

    ct : ohouse

---
+++ src/com/company/codingtest/ohouse/Q1.java
@@ -0,0 +1,9 @@
+package com.company.codingtest.ohouse;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/company/codingtest/ohouse/Q2.java
@@ -0,0 +1,8 @@
+package com.company.codingtest.ohouse;
+
+public class Q2 {
+
+    public static void main(String[] args) {
+
+    }
+}
