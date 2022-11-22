92d3cdfbf39a2d07eaeac90f962604810b893108
HyundeokPark <hpotter1993@naver.com>
2022-11-22 19:57:46 +0900

    tmp save - wip

--- src/com/company/programmers/lv2/Lesson92341.java
+++ src/com/company/programmers/lv2/Lesson92341.java
@@ -12,18 +12,29 @@ import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class Lesson92341 {
-
+    static int defaultFee;
+    static int defaultParkingTime;
+    static int unitTime;
+    static int unitFee;
     public static void main(String[] args) {
         Lesson92341 lesson = new Lesson92341();
-        int[] fees = new int[2];
-        String[] records = new String[2];
 
-        lesson.solution(fees,records);
+
+        int[] fees = {180, 5000, 10, 600};
+        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT",
+            "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+
+        Arrays.stream(lesson.solution(fees,records)).forEach(System.out::println);
     }
     public int[] solution(int[] fees, String[] records) {
         int[] answer = {};
         Map<String,ParkingFee> parkingFees = new HashMap<>();
         List<ParkingFee> answerList = new ArrayList<>();
+
+        defaultParkingTime = fees[0];
+        defaultFee = fees[1];
+        unitTime = fees[2];
+        unitFee = fees[3];
 
         //초기화
         for(String record : records){
@@ -63,7 +74,6 @@ public class Lesson92341 {
         String exitTime = "23:59";
         String carNumber;
         int parkingFees;
-        int[] feeInfo;
 
         public int getParkingFees(){
             return this.parkingFees;
@@ -102,11 +112,11 @@ public class Lesson92341 {
         }
 
         public int calcFeesWithParkingTime(int totalTimeToMinuite){
-            if(totalTimeToMinuite<180){
-                return 5000;
+            if(totalTimeToMinuite<defaultParkingTime){
+                return defaultFee;
             }
-            double v = ((totalTimeToMinuite - 180) / 10);
-            return (int)(5000+(Math.ceil(v)) * 600);
+            double v = ((totalTimeToMinuite - defaultParkingTime) / unitTime);
+            return (int)(defaultFee+(Math.ceil(v)) * unitFee);
         }
 
 
